9e0fde215e93eb7aba90acdbd9d5ecfefe50819b
ex5420_admin <az0422@naver.com>
2024-11-09 00:07:10 +0000

    update

--- modules/nn/modules/head.py
+++ modules/nn/modules/head.py
@@ -4,14 +4,23 @@ import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, Layer, GlobalAveragePooling2D, Conv1D, GlobalAveragePooling1D, Dense
 from tensorflow.keras.models import Sequential
 
+from .layers import Reshape, Conv
+
 class Classify(Layer):
-    def __init__(self, in_channels, classes, act=tf.nn.softmax):
+    def __init__(self, in_channels, classes):
         super().__init__()
         
-        self.conv = Conv2D(classes, 3, 1, activation=act)
-        self.gap = GlobalAveragePooling2D(dtype="float32")
+        self.gap = GlobalAveragePooling2D()
+        self.conv = Sequential([
+            Reshape([1, 1, in_channels]),
+            Conv(in_channels, in_channels, 1, 1),
+            Conv(in_channels, classes, 1, 1),
+            Reshape([classes]),
+            Dense(classes, activation=tf.nn.softmax, dtype=tf.float32),
+        ])
     
     def call(self, x):
-        y = self.conv(x)
-        y = self.gap(y)
+        y = self.gap(x)
+        y = self.conv(y)
+
         return y
