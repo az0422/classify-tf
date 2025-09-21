df7c72c0f2d40e831ee9957626962431afd6eb50
ex5420_admin <az0422@naver.com>
2025-09-21 22:58:27 +0900

    added example of Jupyer Notebook file

--- export.py
+++ export.py
@@ -9,6 +9,10 @@ import tensorflow as tf
 from modules.nn.model import ClassifyModel
 from modules.utils import parse_cfg
 
+class CastingLayer(tf.keras.layers.Layer):
+    def call(self, x, training=None):
+        return tf.cast(x, tf.float32)
+
 def load_cfg(path):
     cfg_path = os.path.join(path, "cfg.yaml")
     cfg = parse_cfg(cfg_path)
@@ -23,9 +27,9 @@ def load_model(path, cfg, image_size):
 
 def load_weights(model, path, epoch):
     if epoch in ("last", "best"):
-        weights_path = os.path.join(path, "weights/%s.keras" % epoch)
+        weights_path = os.path.join(path, "weights/%s.weights.h5" % epoch)
     else:
-        weights_path = os.path.join(path, "weights/epoch-%s.keras" % epoch)
+        weights_path = os.path.join(path, "weights/epoch-%016d.weights.h5" % epoch)
     
     model.load_weights(weights_path)
 
@@ -40,24 +44,11 @@ def make_dirs(path):
 
 def conv_saved_model(path, model):
     export = os.path.join(path, "saved_model")
-    tf.saved_model.save(model, export)
+    export_headless = os.path.join(path, "saved_model_headless")
+    model_headless = tf.keras.models.Model(model.layers_list[0], CastingLayer()(model.layers_list[-2]))
 
-def conv_tflite(path, model):
-    export_fp32 = os.path.join(path, "tflite_fp32.tflite")
-    export_fp16 = os.path.join(path, "tflite_fp16.tflite")
-
-    converter = tf.lite.TFLiteConverter.from_keras_model(model)
-    tflite_model_fp32 = converter.convert()
-
-    converter = tf.lite.TFLiteConverter.from_keras_model(model)
-    converter.target_spec.supported_types = [tf.float16]
-    tflite_model_fp16 = converter.convert()
-
-    with open(export_fp32, "bw") as f:
-        f.write(tflite_model_fp32)
-
-    with open(export_fp16, "bw") as f:
-        f.write(tflite_model_fp16)
+    model.export(export)
+    model_headless.export(export_headless)
 
 def main(path, epoch, image_size):
     print("Load model...")
@@ -70,9 +61,6 @@ def main(path, epoch, image_size):
 
     print("Convert SavedModel...")
     conv_saved_model(export, model)
-
-    print("Convert TFLite...")
-    conv_tflite(export, model)
 
     print("Finished.\nSaved at", export)
 
--- modules/nn/modules/blocks/residual.py
+++ modules/nn/modules/blocks/residual.py
@@ -62,30 +62,3 @@ class SEResNet(Layer):
 
         return x + y
 
-class ResNetT(Layer):
-    def __init__(self, in_channels, out_channels, expand=0.5, kernel=3):
-        assert in_channels == out_channels
-        super().__init__()
-        channels_h = round(out_channels * expand)
-        self.m = Sequential([
-            ConvT(out_channels, channels_h, 1, 1),
-            ConvT(channels_h, channels_h, kernel, 1),
-            ConvT(channels_h, out_channels, 1, 1),
-        ])
-    
-    def call(self, x, training=None):
-        return x + self.m(x, training=training)
-
-class ResNetT2L(Layer):
-    def __init__(self, in_channels, out_channels, expand=1.0, kernel=3):
-        assert in_channels == out_channels
-
-        super().__init__()
-        channels_h = round(out_channels * expand)
-        self.m = Sequential([
-            ConvT(out_channels, channels_h, kernel, 1),
-            ConvT(channels_h, out_channels, 3, 1),
-        ])
-    
-    def call(self, x, training=None):
-        return x + self.m(x, training=training)
