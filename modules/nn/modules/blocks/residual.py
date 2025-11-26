6e3e375460b6f9f8797c1ca7ddefecf63e80ed3c
ex5420_admin <az0422@naver.com>
2025-11-26 09:06:18 +0900

    fix a critical implement issue of ResNet

--- modules/nn/modules/blocks/residual.py
+++ modules/nn/modules/blocks/residual.py
@@ -14,39 +14,11 @@ class ResNet(Layer):
         self.m = Sequential([
             Conv(out_channels, channels_h, 1, 1),
             Conv(channels_h, channels_h, kernel, 1),
-            Conv(channels_h, out_channels, 1, 1),
+            Conv(channels_h, out_channels, 1, 1, act=False),
         ])
     
     def call(self, x, training=None):
-        return x + self.m(x, training=training)
-
-class ResNet2L(Layer):
-    def __init__(self, in_channels, out_channels, expand=1.0, kernel=3):
-        assert in_channels == out_channels
-
-        super().__init__()
-        channels_h = round(out_channels * expand)
-        self.m = Sequential([
-            Conv(out_channels, channels_h, kernel, 1),
-            Conv(channels_h, out_channels, 3, 1),
-        ])
-    
-    def call(self, x, training=None):
-        return x + self.m(x, training=training)
-
-class ResNetSE(Layer):
-    def __init__(self, in_channels, out_channels, n=1, expand=0.5, ratio=16):
-        assert in_channels == out_channels
-        super().__init__()
-
-        self.m = Sequential([ResNet(out_channels, out_channels, expand) for _ in range(n)])
-        self.se = SEBlock(out_channels, out_channels, ratio)
-    
-    def call(self, x, training=None):
-        x = self.m(x, training=training)
-        y = self.se(x, training=training)
-
-        return y
+        return Conv.default_act(x + self.m(x, training=training))
 
 class SEResNet(Layer):
     def __init__(self, in_channels, out_channels, expand=0.5, ratio=16, kernel=3):
